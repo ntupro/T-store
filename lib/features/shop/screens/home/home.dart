7a777075f0be8e1b8600de5e3b9429625eed7bdd
tunmdev <tunmdevv@gmail.com>
2025-06-07 14:27:24 +0700

    Create custom AppBar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            /// Header
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  /// Appbar
+                  THomeAppBar(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
