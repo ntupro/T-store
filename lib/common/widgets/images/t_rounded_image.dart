4cf35d4b39f82b95d8ef238d83d9e30acc24d7d4
tunmdev <tunmdevv@gmail.com>
2025-06-08 15:19:54 +0700

    Add promo carousel slider

---
+++ lib/common/widgets/images/t_rounded_image.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key,
+    required this.imageUrl,
+    this.width,
+    this.height,
+    this.border,
+    this.applyImageRadius = true,
+    this.backgroundColor = TColors.light,
+    this.fit = BoxFit.contain,
+    this.padding,
+    this.isNetworkImage = false,
+    this.borderRadius = TSizes.md,
+    this.onPressed,
+  });
+
+  final double? width, height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: width,
+        height: height,
+        padding: padding,
+        decoration: BoxDecoration(
+          border: border,
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: ClipRRect(
+          borderRadius:
+              applyImageRadius
+                  ? BorderRadius.circular(borderRadius)
+                  : BorderRadius.zero,
+          child: Image(
+            image:
+                isNetworkImage ? NetworkImage(imageUrl) : AssetImage(imageUrl),
+            fit: fit,
+          ),
+        ),
+      ),
+    );
+  }
+}
